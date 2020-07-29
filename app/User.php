69978a3848f7c564f5718f66357b770c303a10f3
bejati <ati.hoseinp@gmail.com>
2020-07-29 19:10:09 +0430

    uploading project

---
+++ app/User.php
@@ -0,0 +1,68 @@
+<?php
+
+namespace App;
+
+use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\Notifiable;
+
+class User extends Authenticatable
+{
+    use Notifiable;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'email', 'password',
+    ];
+
+    /**
+     * The attributes that should be hidden for arrays.
+     *
+     * @var array
+     */
+    protected $hidden = [
+        'password', 'remember_token',
+    ];
+
+    public function isAdmin()
+    {
+        return $this->admin;
+    }
+
+
+     
+    public function address()
+    {
+        return $this->hasMany(Address::class);
+    }
+
+
+    public function orders()
+    {
+        return $this->hasMany(Order::class);
+    }
+
+    public function review()
+    {
+        return $this->hasOne(ProductReview::class);
+    }
+
+
+
+
+
+
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'email_verified_at' => 'datetime',
+    ];
+}
